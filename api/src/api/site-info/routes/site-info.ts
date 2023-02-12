149a939fc39036b9eb19b90145d2518946023b97
Paul Singh <hey@paulsingh.dev>
2023-02-12 10:05:59 +0000

    :card_file_box: Add multiple data types and components which would be required by a standard blog appllication

---
+++ api/src/api/site-info/routes/site-info.ts
@@ -0,0 +1,7 @@
+/**
+ * site-info router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::site-info.site-info');
---
+++ api/src/api/site-info/services/site-info.ts
@@ -0,0 +1,7 @@
+/**
+ * site-info service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::site-info.site-info');
