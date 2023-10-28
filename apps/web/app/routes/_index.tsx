48654f9b61c0cb2240d94ddc36ea3c8432295c13
Paul Singh <hey@paulsingh.dev>
2023-10-28 19:12:52 +0100

    :sparkles: Add some routes

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -1,4 +1,5 @@
-import type { MetaFunction } from "@remix-run/node";
+import { type LoaderFunction, type MetaFunction } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,34 +8,30 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader: LoaderFunction = async () => {
+  const posts = await fetch("http://127.0.0.1:1337/api/posts");
+  const response = await posts.json();
+  const structuredData = response?.data?.map((datum) => ({
+    ...datum.attributes,
+    id: datum.id,
+  }));
+  return structuredData || [];
+};
+
 export default function Index() {
+  const loaderData = useLoaderData<any[]>();
+  console.log(loaderData);
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {loaderData?.map?.((post) => (
+          <li key={post.Slug}>
+            <Link to={`/posts/${post.Slug}`} title={post.Title}>
+              {post.Title}
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
---
+++ apps/web/app/routes/posts.$slug.tsx
@@ -0,0 +1,48 @@
+import {
+  type LoaderFunctionArgs,
+  type LoaderFunction,
+  type MetaFunction,
+} from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import Markdown from "react-markdown";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export const loader: LoaderFunction = async ({
+  request,
+  params,
+}: LoaderFunctionArgs) => {
+  const slug = params.slug;
+  const posts = await fetch(
+    `http://127.0.0.1:1337/api/posts?filters[Slug]=${slug}&populate=*`
+  );
+  const response = await posts.json();
+  const structuredData = response?.data?.map((datum) => ({
+    ...datum.attributes,
+    id: datum.id,
+  }));
+  return structuredData || [];
+};
+
+export default function Index() {
+  const [post] = useLoaderData<any[]>();
+
+  console.log(post);
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <img
+        width={200}
+        height={200}
+        alt={post.FeaturedImage?.data?.attributes.alternativeText || post.Title}
+        src={`http://127.0.0.1:1337${post.FeaturedImage?.data?.attributes?.formats?.small.url}`}
+      />
+      <h1>{post.Title}</h1>
+      <Markdown>{post.Content}</Markdown>
+    </div>
+  );
+}
